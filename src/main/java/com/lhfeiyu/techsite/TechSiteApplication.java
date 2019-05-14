28f1c68a88216937c5d54a2202b4eefcca34c906
yurh <airson_yu@163.com>
2019-05-14 17:34:06 +0800

    init

---
+++ src/main/java/com/lhfeiyu/techsite/TechSiteApplication.java
@@ -0,0 +1,13 @@
+package com.lhfeiyu.techsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechSiteApplication.class, args);
+    }
+
+}
