91e012ef7a6f75717283968dfb28ce80592bb254
gene.a.wang <448497795@qq.com>
2019-05-20 23:00:02 +0800

    添加项目框架

---
+++ api/src/main/java/com/lhfeiyu/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package com.lhfeiyu.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:wangaimin
+ * @date:2019/5/20 22:46
+ * @description:
+ */
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class, args);
+        }
+}
