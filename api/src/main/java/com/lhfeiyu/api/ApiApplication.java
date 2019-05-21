25d7ef7844450f8a1ba7aec74584f2fbe2fb5903
gene.a.wang <448497795@qq.com>
2019-05-21 23:40:27 +0800

    调通api调用business

--- api/src/main/java/com/lhfeiyu/api/ApiApplication.java
+++ api/src/main/java/com/lhfeiyu/api/ApiApplication.java
@@ -1,6 +1,7 @@
 package com.lhfeiyu.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -11,6 +12,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+//SpringBoot默认扫描路径为当前包和子包，无法扫描到business中的包，需要自己添加
+//@ComponentScan(basePackages = {"com.lhfeiyu.api","com.lhfeiyu.business"})
+@ComponentScan(basePackages = "com.lhfeiyu")
 public class ApiApplication {
     public static void main(String[] args){
         SpringApplication.run(ApiApplication.class, args);
