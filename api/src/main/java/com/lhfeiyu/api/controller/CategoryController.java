c2d1e95568a06c21380ecc62dd3be6bbdc758f5f
Gene.A.Wang <448497795@qq.com>
2019-05-21 13:33:45 +0800

    添加测试代码

---
+++ api/src/main/java/com/lhfeiyu/api/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package com.lhfeiyu.api.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author:Gene.A.Wang
+ * @date:2019/5/21
+ * @time:11:45
+ */
+@RestController
+@Api(value = "分类相关接口",description = "提供分类相关Rest API",tags = "分类相关接口")
+@RequestMapping("Category")
+public class CategoryController {
+
+    @RequestMapping("Test")
+    public String Test(){
+        return "hello world";
+    }
+
+    @ApiOperation(value = "获取分类",tags = "分类相关接口")
+    @GetMapping(value="Find/{sysNo}")
+    public String Find(@PathVariable("sysNo") int sysNo){
+        return "Category："+sysNo;
+    }
+
+
+}
