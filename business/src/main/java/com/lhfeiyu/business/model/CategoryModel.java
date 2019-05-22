d9bbb7a71cc92fa80d6a95b6728d0eb0dae097a8
gene.a.wang <448497795@qq.com>
2019-05-22 13:24:08 +0800

    调通api调用business

---
+++ business/src/main/java/com/lhfeiyu/business/model/CategoryModel.java
@@ -0,0 +1,10 @@
+package com.lhfeiyu.business.model;
+
+/**
+ * @author:wangaimin
+ * @date:2019/5/21 23:01
+ * @description:
+ */
+
+public class CategoryModel {
+}
---
+++ business/src/main/java/com/lhfeiyu/business/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.lhfeiyu.business.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author:wangaimin
+ * @date:2019/5/21 22:30
+ * @description:
+ */
+@Service
+public class CategoryService {
+    public String Find(long sysNo){
+        return "Hello Category "+sysNo;
+    }
+}
