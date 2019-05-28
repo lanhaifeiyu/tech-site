c483cc9e18536165e78fe9caf0d2d7ffb7c84ef1
gene.a.wang <448497795@qq.com>
2019-05-28 23:18:11 +0800

    generrator

---
+++ generator/src/main/java/com/lhfeiyu/generator/GeneratorApplication.java
@@ -0,0 +1,50 @@
+package com.lhfeiyu.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author:wangaimin
+ * @date:2019/5/28 22:45
+ * @description:
+ */
+
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+     //   File configFile = new File("generatorConfig.xml");
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classloader.getResourceAsStream("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(inputStream);
+            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
